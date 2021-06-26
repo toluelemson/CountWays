d38706003d45bc607d8e4e811669452eae1f3af5
Toluwani <toluwhani@gmail.com>
2021-06-27 00:51:15 +0300

    Add project files.

---
+++ CountWays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static int fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+
+        // Returns number of ways
+        // to reach n'th stair
+        static int countWays(int s)
+        {
+            return fibonacci(s + 1);
+        }
+
+        static public void Main()
+        {
+            int s = 9;
+            Console.WriteLine("Number of ways = " + countWays(s));
+        }
+    }
+}
